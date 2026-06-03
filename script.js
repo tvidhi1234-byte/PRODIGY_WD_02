3e10bc8864e2ae1965aa8ffbb80ef5e161c31311
tvidhi1234-byte <tvidhi1234@gmail.com>
2026-06-03 23:53:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,124 @@
-// script.js
 
-const themeBtn =
-document.querySelector(".theme-btn");
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
 
-let darkMode = false;
+let laps = [];
 
-themeBtn.addEventListener("click",()=>{
+function timeToString(time){
 
-if(!darkMode){
+    let hrs =
+    Math.floor(time / 3600000);
 
-document.body.style.background =
-"linear-gradient(135deg,#1E1E2F,#2A2A45,#111827)";
+    let mins =
+    Math.floor((time % 3600000) / 60000);
 
-document.body.style.color = "white";
+    let secs =
+    Math.floor((time % 60000) / 1000);
 
-themeBtn.innerHTML="☀️";
+    let ms =
+    Math.floor(time % 1000);
 
-darkMode=true;
+    return (
+        String(hrs).padStart(2,'0')
+        + " : " +
+        String(mins).padStart(2,'0')
+        + " : " +
+        String(secs).padStart(2,'0')
+        + " : " +
+        String(ms).padStart(3,'0')
+    );
+}
 
-}else{
+function startTimer(){
 
-document.body.style.background =
-"linear-gradient(135deg,#F4F0FF,#DDE8FF,#FFFDF8)";
+    startTime =
+    Date.now() - elapsedTime;
 
-themeBtn.innerHTML="🌙";
+    timerInterval =
+    setInterval(function(){
 
-darkMode=false;
+        elapsedTime =
+        Date.now() - startTime;
+
+        document.getElementById(
+            "display"
+        ).innerText =
+        timeToString(elapsedTime);
+
+    },10);
+}
+
+function pauseTimer(){
+
+    clearInterval(timerInterval);
 
 }
 
-});
+function resetTimer(){
+
+    clearInterval(timerInterval);
+
+    elapsedTime = 0;
+
+    laps = [];
+
+    document.getElementById(
+        "display"
+    ).innerText =
+    "00 : 00 : 00 : 000";
+
+    document.getElementById(
+        "laps"
+    ).innerHTML = "";
+
+    document.getElementById(
+        "lapCount"
+    ).innerText = "0";
+
+    document.getElementById(
+        "bestLap"
+    ).innerText = "--";
+}
+
+function addLap(){
+
+    let lapTime =
+    timeToString(elapsedTime);
+
+    laps.push(elapsedTime);
+
+    const lapDiv =
+    document.createElement("div");
+
+    lapDiv.className =
+    "lap-item";
+
+    lapDiv.innerHTML =
+    "Lap " +
+    laps.length +
+    " — " +
+    lapTime;
+
+    document
+    .getElementById("laps")
+    .prepend(lapDiv);
+
+    document.getElementById(
+        "lapCount"
+    ).innerText =
+    laps.length;
+
+    let fastest =
+    Math.min(...laps);
+
+    document.getElementById(
+        "bestLap"
+    ).innerText =
+    timeToString(fastest);
+
+    localStorage.setItem(
+        "veluneLaps",
+        JSON.stringify(laps)
+    );
+}
